9f9bc880f852767b31b2fb0f63393bcbb9b5fc97
Svetoslav Petrov <svetoslav.petrov@ipl.com>
2016-08-02 16:05:48 +0100

    Latest changes

---
+++ www/js/menu.js
@@ -0,0 +1,9 @@
+
+	$( "#menu-icon" ).click(function() {
+  		var x = document.getElementById("myTopnav");
+	    if (x.className === "topnav") {
+	        x.className += " responsive";
+	    } else {
+	        x.className = "topnav";
+	    }
+	});
